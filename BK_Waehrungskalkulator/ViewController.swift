3954d6925cd6874dc1fb4cdd72cf3afea691d102
Benedikt Kurz <bkurz94@gmail.com>
2019-04-23 11:44:31 +0200

    Initial Commit

---
+++ BK_Waehrungskalkulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BK_Waehrungskalkulator
+//
+//  Created by Benedikt Kurz on 23.04.19.
+//  Copyright © 2019 Benedikt Kurz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
